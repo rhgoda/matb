08ddec617d9bbd530734eeaf50225a7e5ed4bd7b
rhgoda <107800912+rhgoda@users.noreply.github.com>
2022-07-03 22:24:49 +0300

    Co-authored-by: Dqu1J <Dqu1J@users.noreply.github.com>

--- src/components/Downloads.svelte
+++ src/components/Downloads.svelte
@@ -7,10 +7,19 @@
     <!-- Section: Design Block -->
     <section class="mb-16 text-gray-800">
         <h2 class="text-3xl font-bold text-center mb-12">Скачать</h2>
-        <div class="flex space-x-2 justify-center">
+        <div class="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-2 gap-4">
             <DownloadButton platform="macos" file="matb_0.1.0_universal.dmg"/>
             <DownloadButton platform="windows" file="matb_0.1.0_universal.msi"/>
-            <DownloadButton platform="linux" file="tochno_ne_poslanie_nahui.txt"/>
+            <DownloadButton platform="linux" file="matb-0.1.0-amd64.deb"/>
+            <DownloadButton platform="portable edition" file="matb-0.1.0-portable.png"/>
+            <DownloadButton platform="android" file="matb-0.1.0-universal.apk"/>
+            <DownloadButton platform="meta quest" file="matb-0.1.0-meta.zip"/>
+            <a href="pwa/index.html" class="col-span-full">
+                <button type="button" class="inline-block w-full px-6 px-10 py-5 bg-blue-600 text-white font-medium text-xs leading-normal uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center">
+                    <img src="assets/web.png" alt="" class="h-10 mr-4">
+                    <span class="mt-2.5">Веб-версия для других платформ (добавить на домашний экран)</span>
+                </button>
+            </a>
         </div>
     </section>
   </div>
